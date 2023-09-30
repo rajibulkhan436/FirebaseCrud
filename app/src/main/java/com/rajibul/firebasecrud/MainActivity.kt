96bed8d91e333412ccc88ff071715e047cebe107
rajibulkhan436 <138011628+rajibulkhan436@users.noreply.github.com>
2023-09-30 13:36:42 +0530

    snapshot added

--- app/src/main/java/com/rajibul/firebasecrud/MainActivity.kt
+++ app/src/main/java/com/rajibul/firebasecrud/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.rajibul.firebasecrud.databinding.ActivityMainBinding
@@ -24,44 +25,32 @@ class MainActivity : AppCompatActivity(), ListInterface {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         recyclerAdapter= RecyclerAdapter(notes,this)
-
-
-        firestore.collection("Users")
-            .get()
-            .addOnSuccessListener {
-              //  System.out.println("in snapshot ${it.documents}")
-                for(items in it.documents){
-                    System.out.println("items ${items.data}")
-                    var firestoreClass= items.toObject(Notes::class.java)?:Notes()
-                    firestoreClass.id=items.id
-                notes.add(firestoreClass)
-                }
-                recyclerAdapter.notifyDataSetChanged()
-            }
         layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.rvNotes.layoutManager = layoutManager
         binding.rvNotes.adapter = recyclerAdapter
 
-
+        getCollectionData()
 
         binding.fabAdd.setOnClickListener {
             var dialog=Dialog(this)
             var dialogBinding:AdditionDialogBinding
             dialogBinding= AdditionDialogBinding.inflate(layoutInflater)
             dialog.setContentView(dialogBinding.root)
-         //   throw RuntimeException("Test Crash")
             dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
             dialog.show()
             dialogBinding.textClock.setOnClickListener {
                 Toast.makeText(this@MainActivity, "TextClock", Toast.LENGTH_SHORT).show()
             }
             dialogBinding.btnAdd.setOnClickListener{
+                getCollectionData()
                 firestore.collection("Users")
                 .add(Notes(title = dialogBinding.etTitle.text.toString(),
                     description = dialogBinding.etDescription.text.toString(),
                     time= dialogBinding.textClock.text.toString()))
                     .addOnSuccessListener {
+                        getCollectionData()
                         Toast.makeText(this,"Data Added",Toast.LENGTH_SHORT).show()
+                        dialog.dismiss()
                     }
                     .addOnFailureListener {
                         Toast.makeText(this,"Data Addition Failed",Toast.LENGTH_SHORT).show()
@@ -69,21 +58,31 @@ class MainActivity : AppCompatActivity(), ListInterface {
                     .addOnCanceledListener {
                         Toast.makeText(this,"Cancelled",Toast.LENGTH_SHORT).show()
                     }
-                notes.clear()
-                firestore.collection("Users")
-                    .get()
-                    .addOnSuccessListener {
-                        //  System.out.println("in snapshot ${it.documents}")
-                        for(items in it.documents){
-                            System.out.println("items ${items.data}")
-                            var firestoreClass= items.toObject(Notes::class.java)?:Notes()
-                            firestoreClass.id=items.id
-                            notes.add(firestoreClass)
-                        }
-                        recyclerAdapter.notifyDataSetChanged()
+            }
+        }
+        firestore.collection("Users").addSnapshotListener { value, error ->
+            if(error != null){
+                return@addSnapshotListener
+            }
+            for(data in value!!.documentChanges){
+                var notes = data.document.toObject(Notes::class.java)
+                notes.id = data.document.id
+                when(data.type){
+                    DocumentChange.Type.ADDED-> {
+                        this.notes.add(notes)
+                    }
+                    DocumentChange.Type.MODIFIED-> {
+                        var indexRemoved=this.notes.indexOfFirst { element->element.id==notes.id }
+                        this.notes.set(indexRemoved, notes)
+
+                    }
+                    DocumentChange.Type.REMOVED-> {
+                        var indexRemoved=this.notes.indexOfFirst { element->element.id==notes.id }
+                        this.notes.removeAt(indexRemoved)
                     }
 
-                dialog.dismiss()
+                }
+                recyclerAdapter.notifyDataSetChanged()
             }
 
         }
@@ -113,6 +112,8 @@ class MainActivity : AppCompatActivity(), ListInterface {
                 .set(updateNotes)
                 .addOnSuccessListener {
                     Toast.makeText(this, "Data Updated", Toast.LENGTH_SHORT).show()
+                    getCollectionData()
+
                 }
                 .addOnFailureListener {
                     Toast.makeText(this, "Data Updation Failed", Toast.LENGTH_SHORT).show()
@@ -120,22 +121,32 @@ class MainActivity : AppCompatActivity(), ListInterface {
                 .addOnCanceledListener {
                     Toast.makeText(this, "Data Updation Cancelled", Toast.LENGTH_SHORT).show()
                 }
-            firestore.collection("Users")
-                .get()
-                .addOnSuccessListener {
-                    //  System.out.println("in snapshot ${it.documents}")
-                    for(items in it.documents){
-                        System.out.println("items ${items.data}")
-                        var firestoreClass= items.toObject(Notes::class.java)?:Notes()
-                        firestoreClass.id=items.id
 
-                        recyclerAdapter.notifyDataSetChanged()
-                    }
+
+//            firestore.collection("Users").addSnapshotListener { value, error ->
+//                if(error!=null)
+//
+//
+//                 }
             recyclerAdapter.notifyDataSetChanged()
             dialog.dismiss()
         }
-
-
     }
+    private fun getCollectionData(){
+        notes.clear()
+        firestore.collection("Users")
+            .get()
+            .addOnSuccessListener {
+                //  System.out.println("in snapshot ${it.documents}")
+                for(items in it.documents){
+                    System.out.println("items ${items.data}")
+                    var firestoreClass= items.toObject(Notes::class.java)?:Notes()
+                    firestoreClass.id=items.id
+                    notes.add(firestoreClass)
+                }
+                recyclerAdapter.notifyDataSetChanged()
+            }
+    }
+}
 
-}}
+
